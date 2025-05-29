3e20b4dced18f36b882a623dfee5d49123366c22
zeinabzaiter <zeinab.zeaiter@etu.univ-amu.fr>
2025-05-29 14:57:28 +0200

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.set_page_config(page_title="Résistance Tests", layout="wide")
+#st.set_page_config(page_title="Résistance Tests", layout="wide")
 
 st.title("🦠 Résistance - Tests antibiotiques")
 
